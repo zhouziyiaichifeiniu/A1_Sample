887808fa914c4269a37529355d9d3dcf1f9a715d
SkyLYnf <skylynf@hotmail.com>
2022-09-29 18:19:46 +0800

    初始化MovieAnalyzer

--- src/MovieAnalyzer.java
+++ src/MovieAnalyzer.java
@@ -1,4 +1,24 @@
+import java.util.List;
+import java.util.Map;
 
 public class MovieAnalyzer {
+    public MovieAnalyzer(String dataset_path){
+
+    }
+    public Map<Integer, Integer> getMovieCountByYear(){
+
+    }
+    public Map<String, Integer> getMovieCountByGenre(){
+
+    }
+    public Map<List<String>, Integer> getCoStarCount(){
+
+    }
+    public List<String> getTopMovies(int top_k, String by){
+
+    }
+    public List<String> getTopStars(int top_k, String by){
+
+    }
 
 }
